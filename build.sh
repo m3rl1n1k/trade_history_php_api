9e6685c5c3a7c629433d21fabe2013bd42dcf197
merl1n1k <bisix21@gmail.com>
2024-08-27 11:16:29 +0200

    Add build script and update environment configurations
    - Create `.env.build` for build configurations.
    - Add `build.sh` script to manage Docker container lifecycle.
    - Change `APP_ENV` to production in `.env`.
    - Update `DATABASE_URL` and `JWT_PASSPHRASE` in `.env`.
    - Modify CORS settings in `nelmio_cors.yaml` for regex support.

--- build.sh
+++ build.sh
@@ -4,22 +4,22 @@ FILE=bulid.lock
 
 if [ -f "$FILE" ]; then
   if [[ $1 == "start" ]]; then
-      docker compose --env-file .env.local up;
+      docker compose --env-file .env.build up;
   fi
   if [[ $1 == "restart" ]]; then
-      docker compose --env-file .env.local restart;
+      docker compose --env-file .env.build restart;
   fi
   if [[ $1 == "stop" ]]; then
-      docker compose --env-file .env.local stop $2;
+      docker compose --env-file .env.build stop $2;
   fi
   if [[ $1 == "php" ]]; then
-      docker compose --env-file .env.local exec -it "php" /bin/bash;
+      docker compose --env-file .env.build exec -it "php" /bin/bash;
   fi
 else
   echo "Container not build. Run command ./container.sh build";
   if [[ $1 == "build" ]]; then
       touch bulid.lock;
-      docker compose --env-file .env.local up --build;
+      docker compose --env-file .env.build up --build;
   else
       echo "Container is build";
       exit;
